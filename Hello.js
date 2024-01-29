d2d656d3636cae0ac7e66a3c9345b1d5e06178b5
Your Name <you@example.com>
2024-01-29 11:22:57 +0700

    This JavaScript Function

---
+++ Hello.js
@@ -0,0 +1,26 @@
+var greeting = "Hello";
+console.log(`${greeting}`);
+
+var age = 100;
+var name = "Makara";
+
+var func = (name,age) => {
+    console.log(`${name} he is ${age}`);
+}
+
+const student = {
+    name1 : "Mam Makara",
+    age1 : 19,
+    gpa : 2.4
+}
+
+console.log(`${student.name1 + " He is " + student.age1 + " years old"}} `);
+
+
+
+func(age,name);
+
+
+
+
+
